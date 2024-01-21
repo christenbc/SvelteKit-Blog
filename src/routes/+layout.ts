ac7602ead2b25ce9b37607b5d8cd0e2bcbced883
Christen <christen.bc@gmail.com>
2024-01-21 21:16:18 +0100

    prerender all pages and rss feed

---
+++ src/routes/+layout.ts
@@ -0,0 +1,7 @@
+import type { LayoutLoad } from './$types';
+
+export const prerender = true // all the links on the site are crawled and the pages are goind to pre-render at build time
+
+export const load = (async () => {
+    return {};
+}) satisfies LayoutLoad;
