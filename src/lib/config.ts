67806bb363e76991dfac490b2110d104a05ed358
Christen <christen.bc@gmail.com>
2024-01-22 16:04:51 +0100

    add generic title and description

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from "$app/environment"
 
-export const title = 'Christen Blom-Dahl'
-export const description = 'Christen Blom-Dahl\'s SvelteKit blog'
+export const title = 'SvelteKit Blog'
+export const description = 'Super fast and elegant SvelteKit blog'
 export const url = dev ? 'http://localhost:5173/' : 'https://christen-bc.web.app/'
