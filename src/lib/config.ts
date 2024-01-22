6e8d7467700123a30f801680f563a4d56fdb0781
Christen <christen.bc@gmail.com>
2024-01-22 16:47:48 +0100

    add firebase hosting

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from "$app/environment"
 
 export const title = 'SvelteKit Blog'
 export const description = 'Super fast and elegant SvelteKit blog'
-export const url = dev ? 'http://localhost:5173/' : 'https://christen-bc.web.app/'
+export const url = dev ? 'http://localhost:5173/' : 'https://sveltekit-blog-2d267.web.app/'
